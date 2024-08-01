759088164296f12541d7c7fc94bd927c621c9aaf
Eric Fuemmeler <efuemmel@.umn.edu>
2024-08-01 17:24:56 -0500

    Made it possible to only optimize

--- mp_tests/elasticity.py
+++ mp_tests/elasticity.py
@@ -13,7 +13,7 @@ class Elasticity(TestDriver,MPMixin):
         add_or_update_property("%s/%s/bulk-modulus-isothermal-npt.edn" %(os.path.dirname(__file__), "property_definitions"))
         add_or_update_property("%s/%s/elastic-constants-isothermal-npt.edn" %(os.path.dirname(__file__), "property_definitions"))
         
-    def post_process(self, orig_atoms):
+    def post_process(self, orig_atoms, only_optimize=False, **kwargs):
         results = kim_edn.loads(self._property_instances)
         
         lengths = self.atoms.cell.lengths().tolist()
@@ -28,29 +28,43 @@ class Elasticity(TestDriver,MPMixin):
             orig_atoms.info["mp-id"], "cell-angles", gt_angles, angles
         )
 
-        t = Tensor.from_voigt(results[0]['elasticity-matrix-raw']['source-value'])
-        elastic_constants = t.convert_to_ieee(
-            AseAtomsAdaptor.get_structure(self.atoms)).voigt
+        if not only_optimize:
 
-        self.insert_mp_outputs(
-            orig_atoms.info["mp-id"],
-            "elastic-constants-ieee",
-            orig_atoms.info["elastic-constants-ieee"],
-            elastic_constants.tolist(),
-            )
+            t = Tensor.from_voigt(results[0]['elasticity-matrix-raw']['source-value'])
+            elastic_constants = t.convert_to_ieee(
+                AseAtomsAdaptor.get_structure(self.atoms)).voigt
 
-        self.insert_mp_outputs(
-            orig_atoms.info["mp-id"],
-            "bulk-modulus-reuss",
-            orig_atoms.info["bulk-modulus-reuss"],
-             results[1]['isothermal-bulk-modulus']['source-value'],
-            )
+            self.insert_mp_outputs(
+                orig_atoms.info["mp-id"],
+                "elastic-constants-ieee",
+                orig_atoms.info["elastic-constants-ieee"],
+                elastic_constants.tolist(),
+                )
+
+            self.insert_mp_outputs(
+                orig_atoms.info["mp-id"],
+                "bulk-modulus-reuss",
+                orig_atoms.info["bulk-modulus-reuss"],
+                results[1]['isothermal-bulk-modulus']['source-value'],
+                )
         self._property_instances = "[]"
+
+    def __call__(self, atoms=None, optimize=False, only_optimize=False, **kwargs):
+        """
+        runs test
+        """
+        super()._setup(atoms, optimize, **kwargs)
+        print ('OO',only_optimize)
+        if only_optimize:
+            return
+        if self.atoms is not None:
+            self.atoms.calc = self._calc
+        self._calculate(**kwargs)
 
 if __name__ == "__main__":
     from mace.calculators import mace_mp
     from utils import mp_species
     model = mace_mp(default_dtype="float64")
     test = Elasticity(model, supported_species=mp_species, db_name='mp.json' )
-    test.mp_tests(job_n=0, n_calcs=10, method="stress-condensed", optimize=True)
+    test.mp_tests(job_n=0, n_calcs=10, method="stress-condensed", optimize=True, only_optimize=True)
 
